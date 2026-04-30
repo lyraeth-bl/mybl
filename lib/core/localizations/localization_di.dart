4f9a31b95241b3dd0ebb5e7626d08829236e3174
Mahsa Nurfarhan Hidayat <farhan@budiluhur.sch.id>
2026-04-30 09:02:56 +0700

    docs(localization): add license and copyright headers
    Add the copyright notice and MIT license header to `localization_storage.dart` and `localization_di.dart`.

--- lib/core/localizations/localization_di.dart
+++ lib/core/localizations/localization_di.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2026 Mahsa Nurfarhan Hidayat / Yayasan Pakarti Luhur. All rights reserved.
+// Use of this source code is governed by a MIT License
+// that can be found in the LICENSE file.
+
 import 'package:hive_ce/hive_ce.dart';
 
 import '../di/get_it_constant.dart';
